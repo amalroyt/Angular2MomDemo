4b8085b1f055cf3b9fd9835dc63a7040ddd68a93
RutujaSaptarshi2504 <rutujasaptarshi736@gmail.com>
2017-02-22 17:29:41 +0530

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 // Import our dependencies
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login';
+import { MeetingComponent } from './meeting';
 import { MeetingListComponent } from './meetingList';
 import { MoreDetailsComponent } from './moreDetails';
 
@@ -8,6 +9,7 @@ import { MoreDetailsComponent } from './moreDetails';
 export const routes: Routes = [
   { path: '',       component: LoginComponent },
   { path: 'login',       component: LoginComponent },
+  { path: 'meeting', component: MeetingComponent },
   { path: 'meetingList',  component: MeetingListComponent },
   { path: 'moreDetails/:id', component: MoreDetailsComponent }
 ];
